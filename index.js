627655a7cb8ad2206c7a113f201e3822a804a4fa
Simon Renoult <srenoult@octo.com>
2017-01-13 10:16:16 +0100

    refactor: stop wrap rating store

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const app = express()
 
-const store = { ratings: [] }
+let ratings = []
 
 app.use(helmet())
 app.use(bodyParser.json())
@@ -14,21 +14,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/ratings', (req, res) => {
-  res.status(200).send(store.ratings)
+  res.status(200).send(ratings)
 })
 
 app.get('/ratings/:id', (req, res) => {
   const id = req.params.id
-  const rating = store.ratings.find(rating => rating.id === id)
+  const rating = ratings.find(rating => rating.id === id)
   if (!rating) return res.sendStatus(404)
   res.send(rating)
 })
 
 app.put('/ratings/:id', (req, res) => {
   const id = req.params.id
-  const rating = store.ratings.find(rating => rating.id === id)
+  const rating = ratings.find(rating => rating.id === id)
   if (!rating) return res.sendStatus(404)
-  store.ratings = store.ratings.map(rating => {
+  ratings = ratings.map(rating => {
     if (rating.id !== id) return rating
     return Object.assign(rating, req.body)
   })
@@ -37,12 +37,12 @@ app.put('/ratings/:id', (req, res) => {
 
 app.post('/ratings', (req, res) => {
   const id = shortid.generate()
-  store.ratings.push(Object.assign(req.body, {id}))
+  ratings.push(Object.assign(req.body, {id}))
   res.location(`/ratings/${id}`).sendStatus(201)
 })
 
 app.delete('/ratings', (req, res) => {
-  store.ratings = []
+  ratings = []
   res.sendStatus(204)
 })
 
